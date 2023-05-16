2d42128061945cdd109b97c405dcabff9e2045e1
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-05-16 12:48:09 -0700

    fix: footer styles

--- apps/web/components/Footer/Footer.tsx
+++ apps/web/components/Footer/Footer.tsx
@@ -77,11 +77,11 @@ export function Footer({
     <footer className="bg-white">
       <div className="mx-auto max-w-7xl overflow-hidden px-6 py-20 sm:py-24 lg:px-8">
         <nav
-          className="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12"
+          className="-mb-6 columns-3 sm:flex sm:justify-center sm:space-x-12"
           aria-label="Footer"
         >
           {navigation.map((item: any) => (
-            <div key={item.name} className="pb-6">
+            <div key={item.name} className="pb-6 text-center">
               <a
                 href={item.href}
                 className="text-sm leading-6 text-gray-600 hover:text-gray-900"
