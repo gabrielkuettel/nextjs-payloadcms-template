010a06d851dfa9f33e5bbf1b67cb51043625986b
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-07-05 09:57:39 -0600

    chore: remove ui package

--- packages/ui/components/Button.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import * as React from 'react'
-
-export const Button = () => {
-  return <button onClick={() => console.log('boop')}>Boop</button>
-}
--- packages/ui/components/Header.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from 'react'
-
-export const Header = ({ text }: { text: string }) => {
-  return <h1 className="text-2xl font-bold text-blue-400">{text}</h1>
-}
