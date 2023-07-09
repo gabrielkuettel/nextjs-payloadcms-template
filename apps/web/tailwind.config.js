ea3841308368635bf63fa9bf2587f75e1dd5ef22
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-07-09 09:23:22 -0600

    chore: remove shared tailwind config

--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,7 +1,29 @@
-// tailwind config is required for editor support
+const { fontFamily } = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
 
-const sharedConfig = require('tailwind-config/tailwind.config.js')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  presets: [sharedConfig]
+  content: [
+    // app content
+    `./**/*.{js,ts,jsx,tsx}`
+    // include packages if not transpiling
+    // "../../packages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.cyan,
+        secondary: colors.red,
+        paper: colors.gray
+      },
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans]
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio')
+  ]
 }
