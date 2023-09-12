c821e2220a1fb8491742588a679519b5b545935f
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-09-12 15:55:20 -0600

    feat: add drop shadow to main menu

--- apps/web/components/MainMenu/MainMenu.tsx
+++ apps/web/components/MainMenu/MainMenu.tsx
@@ -20,7 +20,7 @@ export const MainMenu: React.FC<MenuProps> = ({
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
+    <header className="bg-paper-50 relative z-10 shadow-lg">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
         aria-label="Global"
