df38664a537011436ddea1a0f9c4cbc25f3abaa8
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-09-12 21:07:57 -0600

    fix: centering post content

--- apps/web/components/Post/Post.tsx
+++ apps/web/components/Post/Post.tsx
@@ -111,7 +111,10 @@ export function Post({
             <div className="flex flex-row flex-wrap">{renderSDGs()}</div>
           </div>
         ) : null}
-        <RichText content={post.content} className="mt-4 max-w-2xl sm:mt-8" />
+        <RichText
+          content={post.content}
+          className="mt-4 flex max-w-2xl flex-col items-center sm:mt-8"
+        />
         {post.tags?.length ? (
           <div className="mt-8 sm:mt-16">
             <Tags
