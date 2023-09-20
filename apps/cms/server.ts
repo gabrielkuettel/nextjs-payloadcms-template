9f9f801d6384aca34fad9bfef06732e9e6d33191
Gabriel Kuettel <gabrielkuettel@gmail.com>
2023-09-19 20:44:43 -0600

    feat: add example code

--- apps/cms/server.ts
+++ apps/cms/server.ts
@@ -19,7 +19,33 @@ const start = async () => {
     }
   })
 
+  const router = express.Router()
+
+  // Note: Payload must be initialized before the `payload.authenticate` middleware can be used
+  router.use(payload.authenticate)
+
+  router.get('/', async (req: any, res) => {
+    try {
+      if (req.user) {
+        const posts = await payload.find({
+          collection: 'posts',
+          // Set `overrideAccess` to `false` to ensure that access control is enforced
+          overrideAccess: false,
+          // Pass `user` object to use against access control checks
+          user: req.user
+        })
+
+        return res.send(JSON.stringify(posts))
+      }
+
+      return res.send('Not authenticated')
+    } catch (error: any) {
+      return res.send(error.toString())
+    }
+  })
+
   // Add your own express routes here
+  app.use('/test-route', router)
 
   app.listen(process.env.PORT || process.env.PAYLOAD_LOCAL_PORT)
 }
